538f21f54f81f428a5904472e9347d5ef27657c5
ethadnguyen <tringuyen1382003@gmail.com>
2022-12-23 16:17:36 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+const App = () => {
+  return (
+    <div>
+      <NewExpense />
+      <Expenses />
+    </div>
+  );
+}
+
+export default App;
