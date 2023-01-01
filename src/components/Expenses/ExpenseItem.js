2ba650daf5b9bea4d48cc6448261f85b4edac94c
ethadnguyen <tringuyen1382003@gmail.com>
2023-01-01 16:12:46 +0700

    second commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 const ExpenseItem = ({ id, title, amount, date }) => {
-    const [newTitle, setNewTitle] = useState(title);
-    console.log('re-evaluated');
-    const clickHandler = () => {
-        setNewTitle('Updated')
-        console.log(newTitle);
-    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={date} />
             <div className='expense-item__description'>
-                <h2>{newTitle}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
             </div>
-            <button style={{ padding: '0.5rem', marginLeft: '10px' }} onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
-const Expenses = () => {
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Toilet Paper',
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ];
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+const Expenses = ({ items }) => {
+
+    const [filteredYear, setFilteredYear] = useState('all');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+
+
+    let filteredExpenses = items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
+    if (filteredYear === 'all') {
+        filteredExpenses = items;
+    }
+
+
+
     return (
-        <Card className='expenses'>
-            {expenses.map((item) => (
-                <ExpenseItem key={item.id} title={item.title} id={item.id} amount={item.amount} date={item.date} />
-            ))}
-        </Card>
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
+            </Card>
+        </div>
     )
 }
 
